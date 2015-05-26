658a478c919ce06cb6877e4ec4aec56ef853f13f
Aleksander Matusiak <am347171@students.mimuw.edu.pl>
2015-05-27 00:36:00 +0200

    Added simple tcp server

--- opoznienia.cc
+++ opoznienia.cc
@@ -14,6 +14,9 @@
 #include "boost/program_options.hpp"
 #include <boost/asio.hpp>
 #include <boost/array.hpp>
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
 
 using boost::asio::ip::udp;
 
@@ -33,12 +36,6 @@ bool ssh_service; // configured by -s option
 
 deb(using namespace std;)
 
-std::string make_daytime_string()
-{
-  using namespace std; // For time_t, time and ctime;
-  time_t now = time(0);
-  return ctime(&now);
-}
 
 void udp_delay_server() {
 	try {
@@ -49,7 +46,6 @@ void udp_delay_server() {
 
 		for (;;) {
 			boost::array<uint64_t, 1> recv_buf;
-			uint64_t recv_time;
 			udp::endpoint remote_endpoint;
 			boost::system::error_code error;
 			socket.receive_from(boost::asio::buffer(recv_buf), remote_endpoint, 0, error);
@@ -80,6 +76,76 @@ void udp_delay_server() {
 
 	cout << "wątek\n";
 }
+
+using boost::asio::ip::tcp;
+
+std::string make_daytime_string()
+{
+  using namespace std; // For time_t, time and ctime;
+  time_t now = time(0);
+  return ctime(&now);
+}
+
+class tcp_connection : public boost::enable_shared_from_this<tcp_connection>{
+	public:
+		typedef boost::shared_ptr<tcp_connection> pointer;
+
+		static pointer create(boost::asio::io_service& io_service){
+			return pointer(new tcp_connection(io_service));
+		}
+
+		tcp::socket& socket() {
+			return socket_;
+		}
+
+		void start() {
+			message_ = make_daytime_string();
+
+			boost::asio::async_write(socket_, boost::asio::buffer(message_),
+				boost::bind(&tcp_connection::handle_write, shared_from_this(),
+				boost::asio::placeholders::error,
+				boost::asio::placeholders::bytes_transferred));
+		}
+
+	private:
+		tcp_connection(boost::asio::io_service& io_service)
+		: socket_(io_service) { }
+
+		void handle_write(const boost::system::error_code& /*error*/,
+		  size_t /*bytes_transferred*/) { }
+
+		tcp::socket socket_;
+		std::string message_;
+};
+
+class tcp_server {
+	public:
+		tcp_server(boost::asio::io_service& io_service)
+		: acceptor_(io_service, tcp::endpoint(tcp::v4(), ui_port_num)) {
+			start_accept();
+		}
+
+	private:
+		void start_accept() {
+			tcp_connection::pointer new_connection =
+			  tcp_connection::create(acceptor_.get_io_service());
+
+			acceptor_.async_accept(new_connection->socket(),
+				boost::bind(&tcp_server::handle_accept, this, new_connection,
+				boost::asio::placeholders::error));
+		}
+
+		void handle_accept(tcp_connection::pointer new_connection,
+		  const boost::system::error_code& error) {
+
+			if (!error)
+				new_connection->start();
+
+			start_accept();
+		}
+
+		tcp::acceptor acceptor_;
+};
 
 
 int main (int argc, char *argv[]) {
@@ -114,8 +180,17 @@ int main (int argc, char *argv[]) {
 		ssh_service = true;
 
 	// creating thread for UDP delay server
-	std::thread t(udp_delay_server);
-	t.join();
+	std::thread udp_thread(udp_delay_server);
+
+	try {
+		boost::asio::io_service io_service;
+		tcp_server server(io_service);
+		io_service.run();
+	} catch (std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	udp_thread.join();
 
 	return 0;
 }
